76cf961ede8f862899a7587478d09c20034a37f0
Giriraj2505 <98442107+Giriraj2505@users.noreply.github.com>
2022-09-14 16:05:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/nilarmyint00/filterbotwithcustomname.git /EvaMaria
+  git clone https://github.com/Giriraj2505/autofilter.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
